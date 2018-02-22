dac5200a7a861e787820f6282c45fbc988db06b2
Christian Mesh <cam72cam@gmail.com>
2018-02-21 22:43:27 -0500

    Attempt to add block lookup

---
+++ src/main/java/trackapi/lib/ITrack.java
@@ -0,0 +1,25 @@
+package trackapi.lib;
+
+import net.minecraft.util.math.Vec3d;
+
+public interface ITrack {
+	
+	/**
+	 * The distance between the rails measured in meters
+	 * 
+	 * @see Gauges#STANDARD
+	 * @see Gauges#MINECRAFT
+	 */
+	public double getTrackGauge();
+	
+	/**
+	 * Used by rolling stock to look up their next position.
+	 * 
+	 * @param currentPosition - Current entity or bogey position
+	 * @param rotationYaw - Current entity rotation in degrees
+	 * @param bogieYaw - Current bogey rotation in degrees (set to rotationYaw if unused)
+	 * @param distance - Distanced traveled in meters
+	 * @return The new position of the entity or bogey
+	 */
+	public Vec3d getNextPosition(Vec3d currentPosition, Vec3d motion);
+}
--- src/main/java/trackapi/lib/Util.java
+++ src/main/java/trackapi/lib/Util.java
@@ -1,5 +1,6 @@
 package trackapi.lib;
 
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -7,11 +8,29 @@ import net.minecraft.world.World;
 import trackapi.compat.MinecraftRail;
 
 public class Util {
-	private static ITrackTile getInternalTileEntity(World world, Vec3d pos, boolean acceptMinecraftRails) {
-		BlockPos bp = new BlockPos(Math.floor(pos.xCoord), Math.floor(pos.yCoord), Math.floor(pos.zCoord));
+	private static ITrack getInternalTileEntity(final World world, Vec3d pos, boolean acceptMinecraftRails) {
+		final BlockPos bp = new BlockPos(Math.floor(pos.xCoord), Math.floor(pos.yCoord), Math.floor(pos.zCoord));
+		IBlockState bs = world.getBlockState(bp);
+		
+		if (bs.getBlock() instanceof ITrackBlock) {
+			final ITrackBlock track = (ITrackBlock) bs.getBlock();
+			// Wrap block in ITrack
+			
+			return new ITrack() {
+				@Override
+				public double getTrackGauge() {
+					return track.getTrackGauge(world, bp);
+				}
+				@Override
+				public Vec3d getNextPosition(Vec3d currentPosition, Vec3d motion) {
+					return track.getNextPosition(world, currentPosition, motion);
+				}
+			};
+		}
+		
 		TileEntity te = world.getTileEntity(bp);
-		if (te instanceof ITrackTile) {
-			return (ITrackTile) te;
+		if (te instanceof ITrack) {
+			return (ITrack) te;
 		}
 		if (acceptMinecraftRails) {
 			if (MinecraftRail.isRail(world, bp)) {
@@ -21,8 +40,8 @@ public class Util {
 		return null;
 	}
 	
-	public static ITrackTile getTileEntity(World world, Vec3d pos, boolean acceptMinecraftRails) {
-		ITrackTile track = getInternalTileEntity(world, pos, acceptMinecraftRails);
+	public static ITrack getTileEntity(World world, Vec3d pos, boolean acceptMinecraftRails) {
+		ITrack track = getInternalTileEntity(world, pos, acceptMinecraftRails);
 		if (track != null) {
 			return track;
 		}

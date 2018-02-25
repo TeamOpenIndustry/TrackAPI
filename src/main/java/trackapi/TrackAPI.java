9040930d16231a1b059c39a7856689259913fc52
Christian Mesh <christianmesh1@gmail.com>
2018-02-25 16:42:04 -0500

    Fix accepted MC version

--- src/main/java/trackapi/TrackAPI.java
+++ src/main/java/trackapi/TrackAPI.java
@@ -2,7 +2,7 @@ package trackapi;
 
 import net.minecraftforge.fml.common.Mod;
 
-@Mod(modid = TrackAPI.MODID, version = TrackAPI.VERSION, acceptedMinecraftVersions = "[1.11,1.12)")
+@Mod(modid = TrackAPI.MODID, version = TrackAPI.VERSION, acceptedMinecraftVersions = "[1.10,1.11)")
 public class TrackAPI
 {
     public static final String MODID = "trackapi";

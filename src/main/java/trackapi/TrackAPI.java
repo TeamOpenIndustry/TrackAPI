08019430cb39525ef0a3e6a92db4297c3ba8ab85
Christian Mesh <christianmesh1@gmail.com>
2019-11-10 08:44:38 -0500

    Bump version to 1.2

--- src/main/java/trackapi/TrackAPI.java
+++ src/main/java/trackapi/TrackAPI.java
@@ -6,5 +6,5 @@ import net.minecraftforge.fml.common.Mod;
 public class TrackAPI
 {
     public static final String MODID = "trackapi";
-    public static final String VERSION = "1.1";
+    public static final String VERSION = "1.2";
 }

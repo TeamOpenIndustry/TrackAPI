da06e016c66dbb0d9acc5b61f599ad154028fd57
Goldenfield192 <1437356849@qq.com>
2026-04-26 11:46:20 +0800

    resolve 1.19 conflicts

--- src/main/java/trackapi/lib/Util.java
+++ src/main/java/trackapi/lib/Util.java
@@ -41,7 +41,7 @@ public class Util {
 	}
 
 	private static <T extends ITrack> T getInternalTileEntity(final Level world, Vec3 pos, boolean acceptMinecraftRails, Class<T> type) {
-		final BlockPos bp = new BlockPos(Math.floor(pos.x), Math.floor(pos.y), Math.floor(pos.z));
+		final BlockPos bp = new BlockPos((int) Math.floor(pos.x), (int) Math.floor(pos.y), (int) Math.floor(pos.z));
 		BlockState bs = world.getBlockState(bp);
 
 		if (bs.getBlock() instanceof ITrackBlock) {

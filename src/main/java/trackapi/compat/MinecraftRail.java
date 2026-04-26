9d6c068e61e740d3a3ab6921e8c8471d73ac40aa
Goldenfield192 <1437356849@qq.com>
2026-04-26 12:34:43 +0800

    26.1.2 port

--- src/main/java/trackapi/compat/MinecraftRail.java
+++ src/main/java/trackapi/compat/MinecraftRail.java
@@ -2,11 +2,8 @@ package trackapi.compat;
 
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.core.BlockPos;
-import net.minecraft.world.entity.vehicle.Minecart;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.BaseRailBlock;
-import net.minecraft.world.phys.Vec3;
-import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.RailShape;
 import trackapi.lib.Gauges;
